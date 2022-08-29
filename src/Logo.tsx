c89f166360fde24adc5b015fa8896d4ac2a02ae1
Shane Jonas <jonas.shane@gmail.com>
2022-08-28 23:49:07 -0400

    Fixed to use PUBLIC_URL

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,8 +1,6 @@
-import p from "../package.json";
-
 const Logo = () => (
   <div className="logo inline-block">
-    <img width="22" src={`${p.homepage}/feed_glitch_logo.gif`} alt="logo" />
+    <img width="22" src={`${process.env.PUBLIC_URL}/feed_glitch_logo.gif`} alt="logo" />
   </div>
 )
 
